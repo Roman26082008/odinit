e741a5504fde0cf85f4fc22d4e6387a661f32cc3
RomanPetrenkov <roman.tihka2008@gmail.com>
2024-01-26 14:23:52 +0300

    add Human

--- Test/__init__.py
+++ Test/__init__.py
@@ -1,6 +1,25 @@
-#1коммит - первое сохраненное изменение
-def plus(a, b, c):
-    return a - b - c
+class Human:              #информация о человеке
+    defolt_name = "nonaem"     #начальное положение ноунейм
+    defolt_age = 0          #начальный возраст 0
+    def __init__(self, name = defolt_name, age = defolt_age):         #мы создали значение по умолчанию
+        self.name = name
+        self.age = age       #все здесь выводим
+        self.__money = 0        #отмечаем что у него ноль денег (и мы создали приват параметр)
+        self.__house = None    #отмечаем что у него на начальном этапе нет дома
 
-if __name__ == "__main__": #добавление стрелочки к запуску
-    print(plus(4, 5, 8))
+
+    def info(self, name, age, money, house): #создаем функцию где выводим все о человеке
+        print('имя', name, 'возраст', age, 'денег', money, 'дом', house, sep=" ")
+    @staticmethod #и с помощью статического метода выводим информацию статических параметров
+    def defoult_info():
+        return Human.defolt_name, Human.defolt_age
+
+    def __make_idea(self, home, price):
+        self.__house = home
+        self.__money -= price
+
+    def earn_money(self, plus):
+        self.__money += plus
+    print('Hello,your on credited -->>>', 'руб ')
+
+    def buy_house(self,home, diskord ):
